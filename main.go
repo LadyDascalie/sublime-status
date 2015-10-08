bdd9677ca8ac9a50a71f9e224d986b1629438515
Benjamin Cable <cable.benjamin@gmail.com>
2015-10-08 15:53:30 +0200

    Added emojis :D:D

--- main.go
+++ main.go
@@ -2,27 +2,30 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
-	"strings"
 	"time"
+
+	"gopkg.in/kyokomi/emoji.v1"
 )
 
 func main() {
-	// getDir()
+	countDirtyFiles()
+	getDir()
 	getBranch()
 	getTime()
 }
 
-func getBranch() {
+func getBranch() (output string, err error) {
 	cmd := exec.Command("bash", "-c", `git branch | grep "*"`)
 	stdout, err := cmd.Output()
-	output := string(stdout)
+	output = string(stdout)
 	if err != nil {
 		fmt.Print("Not a Git repo ◀")
-		return // Let it run despite the error
+		return output, err // Let it run despite the error
 	} else {
-		fmt.Print(output, "◀ ")
+		clock := emoji.Sprint(":clock1:")
+		fmt.Print(output, clock, "◀ ")
+		return output, err
 	}
 }
 
@@ -34,14 +37,28 @@ func getTime() {
 }
 
 func getDir() {
-	dir, err := os.Getwd()
+	cmd := exec.Command("bash", "-c", `echo "${PWD##*/}"`)
+	stdout, err := cmd.Output()
+	output := string(stdout)
 	if err != nil {
-		fmt.Print("Couldn't get path")
+		fmt.Print("Err. getting folder name")
 	} else {
-		fmt.Print("|| ", dir, " ||")
+		fmt.Print("|| ", output, "|| ")
 	}
 }
 
-func minifyPath(path string) {
-	strings.Split(path, "/")
+func countDirtyFiles() {
+	cmd := exec.Command("bash", "-c", `git status | grep modified: | wc -l`)
+	stdout, err := cmd.Output()
+	output := string(stdout)
+	if err != nil {
+		fmt.Print("Couldnt read dir.")
+	}
+	if output == "0" {
+		fmt.Print("Clean dir. ")
+	} else {
+		rose := emoji.Sprint(":rose:")
+		fmt.Print(rose, output, "files changed ")
+	}
+
 }
